0e9f732cfb698cfe93f67c5f25c3da7599cd21c6
imlishner-com <77021886+imlishner-com@users.noreply.github.com>
2021-01-05 21:56:32 +0200

    first commit

--- Pages/Program.cs
+++ Pages/Program.cs
@@ -11,7 +11,7 @@ namespace Pages
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! ");
             Console.ReadKey();
         }
     }
